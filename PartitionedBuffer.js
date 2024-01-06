9e3c8bd7e236a8c3608cbb54a09557574d21dbfb
Allen Ng <allen@allenng.com>
2024-01-06 18:52:54 -0500

    Parallelization using WebWorkers
    It works!  It finally WORKS!
    
    This object is meant to implement a threading pattern common in Java,
    but that I've not seen in JavaScript.  The interface is the same as a
    standard array (and can be extended to implement any missing Array
    features), but internally the data is stored in typed arrays, striped
    across multiple ArrayBuffers which are created during object
    construction.
    
    The entire point of all of this is so that it is possible to call .map()
    and have it run concurrently.  Data is striped across the internal
    buffers which are then transferred to Web Workers to be processed and
    then transferred back to the main thread.  This is the magic sauce that
    makes parallel processing faster.

---
+++ PartitionedBuffer.js
@@ -0,0 +1,157 @@
+/**
+ *	PartitionedBuffer
+ *	@param {TypedArrayConstructor} type - The type of the typed array (e.g., Int32Array).
+ *	@param {number} size - The total number of elements that the buffer will hold.
+ *	@constructor
+ *
+ * @fileOverview PartitionedBuffer module.
+ * @module PartitionedBuffer
+ * @version 1.0.0
+ * @description A Striped Array of Concurrent Buffers (SACB)
+ * @author <accounts@stupid-genius.com>
+ * @license GPL-3.0
+ * @since 2024-01-06
+ */
+function PartitionedBuffer(type, size){
+	if(!new.target){
+		return new PartitionedBuffer(...arguments);
+	}
+	/**
+	 * Validates if the type is a valid typed array.
+	 * @throws {Error} Throws an error if the type is not a valid typed array.
+	 */
+	if(!['Int32Array'].includes(type.name)){
+		throw new Error(`${type} is not a valid typed array`);
+	}
+
+	const segments = [];
+	const threads = navigator.hardwareConcurrency;
+	const bufSize = Math.ceil(size/threads * type.BYTES_PER_ELEMENT);
+	const indexes = new Set();
+	let nextIndex = 0;
+	for(let i=0; i<threads; i++){
+		const buffer = new ArrayBuffer(bufSize);
+		segments.push(new type(buffer));
+	}
+
+	/**
+	 * Returns the buffer and offset corresponding to the given index.
+	 * @param {number} i - The index to convert to coordinates.
+	 * @returns {number[]} The buffer and offset [buf, off].
+	 */
+	function indexToCoord(i){
+		return [i%threads, Math.floor(i/threads)];
+	}
+
+	Object.defineProperties(this, {
+		/**
+		 * Iterator for the PartitionedBuffer.
+		 * @returns {Generator} A generator for iterating over the buffer.
+		 */
+		[Symbol.iterator]: {
+			value: function*(){
+				for(const index of Array.from(indexes).sort((a, b) => a - b)){
+					yield this[index];
+				}
+			}
+		},
+		length: {
+			get: function(){
+				return indexes.size;
+			}
+		},
+		/**
+		 * Maps each element in the buffer using a transform function.  This is a volatile operation that modifies the
+		 * data in this object.
+		 * @param {Function} transform - The transform function applied to each element.
+		 * @returns {Promise} A promise that resolves to the returned values from the individual buffers.  This can be
+		 * used for communication with the main thread.
+		 */
+		map: {
+			value: function(transform){
+				const blob = new Blob([`self.onmessage=function(msg){
+					const response = (${transform.toString()})(msg.data);
+					postMessage(response, [msg.data.buffer]);
+				}`], {type: 'application/javascript'});
+				const blobURL = URL.createObjectURL(blob)
+				const workers = [];
+
+				for(let i=0; i<threads; i++){
+					workers.push(new Promise((res, _rej) => {
+						const worker = new Worker(blobURL);
+						worker.onmessage = function(msg){
+							segments[i] = new type(msg.data.buffer);
+							res(msg.data);
+						};
+						worker.postMessage(segments[i], [segments[i].buffer]);
+					}));
+				}
+				return Promise.all(workers).then(result => result.flat());
+			}
+		},
+		/**
+		 * Adds elements to the end of the buffer.
+		 * @param {...*|Array} elements - The elements to add to the buffer.
+		 * @returns {number} The new size of the buffer.
+		 */
+		push: {
+			value: function(...elements){
+				if(Array.isArray(elements[0])){
+					elements = elements[0];
+				}
+				elements.forEach(element => {
+					const newIndex = nextIndex++;
+					indexes.add(newIndex);
+
+					const [buf, off] = indexToCoord(newIndex);
+					// console.debug('push', buf, off, element);
+					segments[buf][off] = element;
+				});
+
+				return indexes.size;
+			}
+		}
+	});
+
+	/**
+	 * Proxy for accessing elements in the buffer by index.
+	 * @type {Proxy}
+	 */
+	const indexProxy = new Proxy(this, {
+		get: function(target, prop){
+			if(typeof prop === 'string' && !isNaN(prop)){
+				const index = +prop;
+				if(Number.isInteger(index) && index >= 0 && index < size) {
+					const [buf, off] = indexToCoord(index);
+					// console.debug('get', buf, off, segments[buf][off]);
+					return segments[buf][off];
+				}
+			}else{
+				// console.debug(prop);
+				return target[prop];
+			}
+		},
+		set: function(_target, prop, value){
+			const index = +prop;
+
+			if(!Number.isInteger(index) || index < 0 || index >= size){
+				console.error(`Invalid (set) index ${prop}`);
+				return false;
+			}
+
+			if(indexes.has(index)){
+				console.warn(`Element at index ${index} already exists. Overwriting.`);
+			}else{
+				indexes.add(index);
+				nextIndex = Math.max(nextIndex, index+1);
+			}
+			const [buf, off] = indexToCoord(index);
+			// console.debug('set', buf, off, value);
+			segments[buf][off] = value;
+			return true;
+		}
+	});
+	return indexProxy;
+}
+
+export default PartitionedBuffer;
---
+++ spec.js
@@ -0,0 +1,105 @@
+const { assert, expect } = require('chai');
+import PartitionedBuffer from './PartitionedBuffer';
+
+describe('PartitionedBuffer', function(){
+	it('should properly construct', function(){
+		const parbuf = new PartitionedBuffer(Int32Array, 120);
+		expect(parbuf).instanceOf(PartitionedBuffer);
+	});
+	it('should correctly calculate length', function(){
+		const parbuf = new PartitionedBuffer(Int32Array, 120);
+		parbuf[0] = 42;
+		parbuf[1] = 24;
+
+		expect(parbuf.length).to.equal(2);
+	});
+	it('should allow access to elements by index', function(){
+		const parbuf = new PartitionedBuffer(Int32Array, 120);
+		parbuf[0] = 42;
+		expect(parbuf[0]).equal(42);
+	});
+	it('should iterate over elements', function(){
+		const parbuf = new PartitionedBuffer(Int32Array, 120);
+		parbuf[0] = 42;
+		parbuf[1] = 24;
+
+		const result = Array.from(parbuf);
+		expect(result).deep.equal([42, 24]);
+	});
+	it('should allow pushing elements', function(){
+		const parbuf = new PartitionedBuffer(Int32Array, 120);
+		parbuf.push(42);
+		parbuf.push(24);
+		parbuf.push(16, 32);
+		parbuf.push([1, 2]);
+
+		const result = Array.from(parbuf);
+		expect(result).deep.equal([42, 24, 16, 32, 1, 2]);
+	});
+	it('should allow mapping over elements', async function(){
+		const parbuf = new PartitionedBuffer(Int32Array, 120);
+		parbuf.push(42, 24, 16, 32);
+
+		await parbuf.map(data => {
+			return data.map((e) => e * 2);
+		});
+		expect(Array.from(parbuf)).deep.equal([84, 48, 32, 64]);
+	});
+	it('should be faster than non-concurrent map', async function(){
+		this.timeout(10000);
+		const maxInt = 2**31 - 1;
+		const size = 10000*navigator.hardwareConcurrency;
+		const min = Math.floor(maxInt/10);
+		const max = min*2;
+
+		console.log(`Comparing performance for ${size.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} random integers`);
+		// console.debug(`in range (${min}, ${max})`);
+		const elem = Array.from(Array(size).keys()).map(_i => Math.round(Math.random()*(max-min)+(min)));
+
+		let start = performance.now();
+		const parbuf = new PartitionedBuffer(Int32Array, size);
+		parbuf.push(elem);
+		// const setup = performance.now()-start;
+		// console.debug(`setup: ${Math.round(setup)}ms`);
+
+		// console.debug('starting non-concurrent test');
+		start = performance.now();
+		const nonConResult = elem.map((x) => {
+			let result = 0;
+			const iterations = 25;
+			for(let i = 0; i < iterations; i++){
+				for(let j = 0; j < iterations; j++){
+					result = x * (Math.sin(result * i) + Math.cos(result * j)) + x;
+				}
+			}
+			return Math.floor(result);
+		});
+		const nonConcurrent = performance.now()-start;
+		console.info(`non-concurrent: ${Math.round(nonConcurrent)}ms`);
+
+		// console.debug('starting concurrent test');
+		start = performance.now();
+		await parbuf.map(data => {
+			return data.map((x) => {
+				let result = 0;
+				const iterations = 25;
+				for(let i = 0; i < iterations; i++){
+					for(let j = 0; j < iterations; j++){
+						result = x * (Math.sin(result * i) + Math.cos(result * j)) + x;
+					}
+				}
+				return Math.floor(result);
+			});
+		});
+		const concurrent = performance.now()-start;
+		console.info(`concurrent: ${Math.round(concurrent)}ms`);
+		console.info(`speed up: ${(nonConcurrent/concurrent).toFixed(2)}`);
+
+		assert(concurrent < nonConcurrent);
+		for(let i=0; i<100; ++i){
+			const spotCheck = Math.floor(Math.random() * size);
+			// console.debug(`\n array: ${nonConResult[i]}\nparbuf: ${parbuf[i]}`);
+			assert(nonConResult[spotCheck] === parbuf[spotCheck]);
+		}
+	});
+});
